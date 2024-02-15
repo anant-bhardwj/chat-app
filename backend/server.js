40ce2a1360af0e0b848533234cec72183834f28b
anant-bhardwj <bhardwaj.anant2001@gmail.com>
2024-02-15 17:16:38 +0530

    Connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.get("/", (req, res) => {
   res.send("HELLO WOOOOOHHOOOOOO!!");
 });
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
