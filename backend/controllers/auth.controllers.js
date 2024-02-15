12ec62f96cd6f245299e338055d5b893177f6e97
anant-bhardwj <bhardwaj.anant2001@gmail.com>
2024-02-15 17:55:55 +0530

    controller for signup of newUser defined

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,12 +1,49 @@
-export const signup = (req, res) => {
-    console.log("signupUser");
-    res.send("SIGNUPPPP");
-}
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, userName, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match" });
+    }
+
+    const user = await User.findOne({ userName });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    //HASH PASSWORD HERE
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+
+    const newUser = new User({
+      fullName,
+      userName,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      userName: newUser.userName,
+      profilePic: newUser.profilePicture,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export const login = (req, res) => {
-    console.log("loginUser");
-}
+  console.log("loginUser");
+};
 
 export const logout = (req, res) => {
-    console.log("logoutUser");
-}
+  console.log("logoutUser");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("HELLO WOOOOOHHOOOOOO!!");
-});
+app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("HELLO WOOOOOHHOOOOOO!!");
+// });
 
 app.listen(PORT, () => {
   connectToMongoDB();
