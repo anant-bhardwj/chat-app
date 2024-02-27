675f8e2edd76c75241e32183cdedd89937be88ee
anant-bhardwj <bhardwaj.anant2001@gmail.com>
2024-02-28 00:39:09 +0530

    Sidebar components added
    Changes to be committed:
    	new file:   frontend/src/components/sidebar/Conversation.jsx
    	new file:   frontend/src/components/sidebar/Conversations.jsx
    	new file:   frontend/src/components/sidebar/LogoutButton.jsx
    	modified:   frontend/src/components/sidebar/SearchInput.jsx
    	modified:   frontend/src/components/sidebar/Sidebar.jsx

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -9,7 +9,7 @@ const SearchInput = () => {
         placeholder="Search.."
         className="input input-brodered rounded-full"
       />
-      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+      <button type="submit" className="btn btn-circle text-white">
         <IoSearchSharp className="w-6 h-6 outline-none" />
       </button>
     </form>
